f61fb02a57d2e31be29c5a5d6098ba01e22de299
Kennedy <nofuequequise@gmail.com>
2022-08-16 17:33:13 -0400

    Cap18

---
+++ modulos/os.js
@@ -0,0 +1,21 @@
+const os = require('os');
+
+//console.log(os.arch());
+//console.log(os.platform());
+//console.log(os.cpus().length);
+//console.log(os.constants);
+/* const SIZE = 1024;
+function kb(byte) {return byte / SIZE}
+function mb(byte) {return kb(byte) / SIZE}
+function gb(byte) {return mb(byte) / SIZE}
+console.log(os.freemem());
+console.log(kb(os.freemem()));
+console.log(mb(os.freemem()));
+console.log(gb(os.freemem()));
+console.log(gb(os.totalmem())); */
+
+//console.log(os.homedir())
+
+//console.log(os.tmpdir())
+//console.log(os.hostname())
+console.log(os.networkInterfaces())
