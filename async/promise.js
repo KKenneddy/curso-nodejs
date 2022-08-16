f59e753c7ac4c38adf9b33f7fdfeab0c8caccf1b
Kennedy <nofuequequise@gmail.com>
2022-08-16 13:05:17 -0400

    Cap9

---
+++ async/promise.js
@@ -0,0 +1,43 @@
+function hola(nombre) {
+    return new Promise(function (resolve, reject) {
+        setTimeout(function () {
+            console.log('Hola', nombre);
+            resolve(nombre);
+        }, 1500);
+    });
+}
+
+function hablar(nombre) {
+    return new Promise((resolve, reject) => {
+        setTimeout(function () {
+            console.log('Bla bla bla bla ...');
+            //resolve(nombre);
+            reject('Hay un error')
+        }, 1000);
+    })
+}
+
+function adios(nombre) {
+    return new Promise((resolve, reject) => {
+        setTimeout(function () {
+            console.log('Adios', nombre);
+            resolve();
+        }, 1000);
+    });
+}
+
+//--
+console.log('iniciando el proceso..');
+hola('Kennedy')
+    .then(hablar)
+    .then(hablar)
+    .then(hablar)
+    .then(hablar)
+    .then(adios)
+    .then((nombre) => {
+        console.log('terminado el proceso');
+    })
+    .catch(err => {
+        console.error('Ha ocurrido un error:')
+        console.error(err)
+    })
